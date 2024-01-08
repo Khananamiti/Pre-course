2e3b70bedc775d07991b4bab734ab41c47c9d39f
Khananamiti <khananova_kha@mail.ru>
2024-01-08 19:34:34 +0600

    added info-technologies

--- 01/01-lesson-js/index4.js
+++ 01/01-lesson-js/index4.js
@@ -1,18 +1,30 @@
 // data
-const title = 'it-incubator';
-const graduatesCount = 2000;
-const areYouChampion = true;
+const info = {
+    title: 'it-incubator',
+    graduatesCount: 2000,
+    areYouChampion: true
+}
+
+// data
+const info2 = {
+    title: 'it-incubator',
+    graduatesCount: 2000,
+    areYouChampion: true,
+    technologies: ['Front', 'Back', 'DevOps']
+}
 
 // render
 let pageTitleElement = document.createElement('h1');
-pageTitleElement.innerText = title;
+pageTitleElement.innerText = info.title;
 document.body.append(pageTitleElement);
 
 let graduatesCountElement = document.createElement('input');
-graduatesCountElement.value = graduatesCount;
+graduatesCountElement.value = info.graduatesCount;
 document.body.append(graduatesCountElement);
 
 let areYouChampionElement = document.createElement('input');
 areYouChampionElement.type = 'checkbox';
-areYouChampionElement.checked = areYouChampion;
+areYouChampionElement.checked = info.areYouChampion;
 document.body.append(areYouChampionElement);
+
+
